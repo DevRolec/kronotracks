01ce109c8fb684b481a94deb80700f812223eb96
njimogu wisdom <njimoguwisdom@gmail.com>
2025-06-11 09:37:00 +0100

    footer and form done

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,18 @@
-import Navbar from "@/components/Navbar"
-import Hero from "@/components/Hero"
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import Footer from "@/components/Footer";
+import Form from "@/components/Form";
 export default function Home() {
   return (
     <div className="">
-      <Navbar/>
-      <Hero/>
+      <Navbar />
+      <Hero />
       <h1 className="text-4xl font-bold text-dark p-4">KRONOTRACKS PROJECT</h1>
       <p className="text-lg mt-4">Welcome to KronoTracks</p>
+
+      <Form />
+
+      <Footer />
     </div>
-  )
+  );
 }
