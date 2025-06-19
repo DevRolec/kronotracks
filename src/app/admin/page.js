3d52cde161933a391d637895fceab3feb06365d6
njimogu wisdom <njimoguwisdom@gmail.com>
2025-06-19 14:03:57 +0100

    ff

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -9,7 +9,7 @@ import Sidebar from "../../components/Sidebar";
 const page = () => {
   const [menuVisible, setMenuVisible] = useState(true);
   return (
-    <div className="relative">
+    <div className="relative ">
       <Sidebar
         className={`fixed top-0 left-0 h-screen w-1/2 bg-black/70 shadow-md z-50 transform transition-transform duration-300 ease-in-out  bottom-0 ${
           menuVisible ? "translate-x-0" : "-translate-x-full"
